f20af82ac9f35f4320afe0173b2666f3e5c0ccb6
Nick Stielau <nick.stielau@gmail.com>
2023-10-20 10:05:14 -0700

    Adding test for freePeriod

--- tests/library.spec.js
+++ tests/library.spec.js
@@ -1,4 +1,5 @@
-import { appropriateFreePeriods } from './../src/bg/library.js';
+import { appropriateFreePeriods,
+         determineFreePeriods } from './../src/bg/library.js';
 
 describe('appropriateFreePeriods function', function () {
   it('Return false for short periods', function () {
@@ -6,7 +7,7 @@ describe('appropriateFreePeriods function', function () {
       start: new Date(),
       end: new Date() + 5*60*1000
     });
-    expect(result).toBe(false); 
+    expect(result).toBe(false);
   });
 
   it('Return false for weekends', function () {
@@ -14,7 +15,7 @@ describe('appropriateFreePeriods function', function () {
       start: new Date('11/21/2023'),
       end: new Date('11/21/2023')
     });
-    expect(result).toBe(false); 
+    expect(result).toBe(false);
   });
 
   it('Return false for early preiods', function () {
@@ -22,14 +23,33 @@ describe('appropriateFreePeriods function', function () {
       start: new Date('11/21/2023 06:00:00'),
       end: new Date('11/21/2023 06:30:00')
     });
-    expect(result).toBe(false); 
-  });  
+    expect(result).toBe(false);
+  });
 
   it('Return false for late preiods', function () {
     const result =  appropriateFreePeriods({
       start: new Date('11/21/2023 23:00:00'),
       end: new Date('11/21/2023 23:30:00')
     });
-    expect(result).toBe(false); 
-  });    
+    expect(result).toBe(false);
+  });
 });
+
+describe('determineFreePeriods function', function () {
+  it('Return an empty array with empty array input', function () {
+    const result = determineFreePeriods([{
+        start: {dateTime: '2023-10-20T09:00:00Z'},
+        end: {dateTime: '2023-10-20T10:00:00Z'},
+      },{
+        start: {dateTime: '2023-10-20T11:00:00Z'},
+        end: {dateTime: '2023-10-20T12:00:00Z'},
+      },
+    ]);
+    expect(result[0]).toStrictEqual({
+      "end": "2023-10-20T11:00:00.000Z",
+      "start": "2023-10-20T10:00:00.000Z",
+     });
+  });
+});
+
+
